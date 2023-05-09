d340d122f8cf5fae466497c7b6e2e2d679185073
Juan Christian <Juanchristian2002@outlook.com>
2023-05-09 10:22:21 +0100

    Get Activation hook working
    Executing the sql solely inside php

--- wp-app/wp-content/plugins/get-manuals/getManuals.php
+++ wp-app/wp-content/plugins/get-manuals/getManuals.php
@@ -21,18 +21,20 @@ function display_serial_number_search_form()
 
     if (isset($_POST['submit'])) {
         $serial_number = $_POST['serial_number'];
-        $model = $_POST['model'];
-        $manual = get_manual_by_serial_number($serial_number, $model);
+        $product_code = $_POST['product_code'];
+        $manual = get_manual_by_serial_number_and_product_code($serial_number, $product_code);
         if (!$manual) {
-            $error = 'Serial number not found. Please enter a new one.';
+            $error = 'Serial number and/or product code not found. Please enter valid ones.';
         }
     }
 ?>
     <form method="post" action="">
         <label for="serial_number">Enter Serial Number:</label>
         <input type="text" name="serial_number" id="serial_number">
-        <label for ="model">Enter Model:</label>
-        <input type="text" name="model" id="model">
+        <br><br>
+        <label for="product_code">Enter Product Code:</label>
+        <input type="text" name="product_code" id="product_code">
+        <br><br>
         <input type="submit" name="submit" value="Search">
     </form>
 
@@ -40,51 +42,106 @@ function display_serial_number_search_form()
         <p><?php echo $error; ?></p>
     <?php } ?>
 
-    <?php if ($manual) { ?>
-        <h3>Manual Details</h3>
-        <ul>
-            <li><strong>Serial Number:</strong> <?php echo $manual->SerialNumber; ?></li>
-            <li><strong>Manual:</strong> <a href="<?php echo $manual->Manual; ?>" target="_blank"><?php echo $manual->Manual; ?></a></li>
-        </ul>
-    <?php } ?>
+    <?php ?>
+    <h3>Manual Details</h3>
+    <ul>
+        <li><strong>Serial Number:</strong> <?php echo $manual->SerialNumber; ?></li>
+        <li><strong>Product Code:</strong> <?php echo $manual->ProductCode; ?></li>
+        <li><strong>Manual:</strong> <a href="<?php echo $manual->Manual; ?>" target="_blank"><?php echo $manual->Manual; ?></a></li>
+    </ul>
+    <?php ?>
 <?php
 }
 
 
 //Function to search the DB for the serial number and return the manual
-function get_manual_by_serial_number($date, $model)
+//Function to search the DB for the serial number and return the manual
+function get_manual_by_serial_number_and_product_code($date_id, $product_code)
 {
     global $wpdb;
-    $table_name = 'tblManuals';
-
-    //get the manual from the database using the serial number and model
-    $manual = $wpdb->get_row("SELECT m.pdf
-    FROM $table_name m
-    INNER JOIN tblJoin j ON m.ManualID = j.ManualID
-    INNER JOIN tblDate d ON j.DateID = d.DateID
-    WHERE d.Date = '$date'
-      AND j.ProductID = '$model';
-    ");
+    $manual = $wpdb->get_row($wpdb->prepare(
+        "
+    SELECT tblManuals.filename 
+    FROM tblJoin 
+    INNER JOIN tblManuals ON tblJoin.ManualID = tblManuals.ManualID 
+    WHERE tblJoin.DateID = '$date_id' AND tblJoin.ProductID = '$product_code';
+    ",
+        $date_id,
+        $product_code
+    ));
+    echo $wpdb->last_query;
+    echo '<br>';
+    echo $manual;
     return $manual;
 }
 
-// hook
+// activation hook
+
 register_activation_hook(__FILE__, 'my_plugin_activate');
 function my_plugin_activate()
-{ 
-  global $wpdb;
-  //get sql file and store it as a string
-  $sql = file_get_contents( plugin_dir_path( __FILE__ ) . 'sql/tblManuals.sql');
-  //replace the prefix with the wordpress prefix
-  $sql = str_replace('wp_', $wpdb->prefix, $sql);
-  //split the sql file into separate queries
-  $sql = explode(';', $sql);
-  //require the upgrade file
-  require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-  //run each query
-  foreach($sql as $query){
-    $wpdb->query($query);
-  }
+{
+    global $wpdb;
+    // Drop tables if they exist
+    $wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}tblJoin");
+    $wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}tblManuals");
+    $wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}tblProduct");
+    $wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}tblDate");
+
+    // Create tables if they don't exist
+    $wpdb->query("CREATE TABLE IF NOT EXISTS {$wpdb->prefix}tblDate (
+      DateID varchar(6) PRIMARY KEY,
+      Date date
+    )");
+
+    $wpdb->query("CREATE TABLE IF NOT EXISTS {$wpdb->prefix}tblProduct (
+      ProductID VARCHAR(30) PRIMARY KEY,
+      ProductName VARCHAR(255),
+      ProductDescription TEXT
+    )");
+
+    $wpdb->query("CREATE TABLE IF NOT EXISTS {$wpdb->prefix}tblManuals (
+      ManualID varchar(15) PRIMARY KEY,
+      filename VARCHAR(255),
+      pdf LONGBLOB
+    )");
+
+    $wpdb->query("CREATE TABLE IF NOT EXISTS {$wpdb->prefix}tblJoin (
+      DateID VARCHAR(6),
+      ProductID VARCHAR(30),
+      ManualID varchar(15),
+      PRIMARY KEY (DateID, ProductID)
+    )");
+
+    // Insert data
+    $wpdb->query("INSERT INTO {$wpdb->prefix}tblDate (DateID, Date)
+    VALUES
+      ('010501', '2001-05-01'),
+      ('011201', '2001-12-01'),
+      ('020501', '2002-05-01')");
+
+    $wpdb->query("INSERT INTO {$wpdb->prefix}tblProduct (ProductID, ProductName, ProductDescription)
+    VALUES
+      ('C08-001-001-01-1-1', 'Air 5 Oxygen & Carbon Dioxide', '0-100% O2 + 0-5% CO2 ambient monitor with diffusion sensor (surface mount)'),
+      ('C05-35-02', 'Air 5 Hypobaric Oxygen & Pressure', '0-40% O2 & 10-1300mbar (A) (Panel Mount)'),
+      ('SAT-35-14', 'Air 5 Converter', 'USB to RS485 Converter')");
+
+    $wpdb->query("INSERT INTO {$wpdb->prefix}tblManuals (ManualID, filename, pdf)
+    VALUES 
+      ('MAN001', 'Man1.pdf', NULL),
+      ('MAN002', 'Man2.pdf', NULL)");
+
+    // // Load pdf files
+    // $man1_pdf = file_get_contents('/public_html/pdfs/Man1.pdf');
+    // $man2_pdf = file_get_contents('/public_html/pdfs/Man2.pdf');
+
+    // // Insert pdf data
+    // $wpdb->query($wpdb->prepare("UPDATE {$wpdb->prefix}tblManuals SET pdf = %s WHERE ManualID = 'MAN001'", $man1_pdf));
+    // $wpdb->query($wpdb->prepare("UPDATE {$wpdb->prefix}tblManuals SET pdf = %s WHERE ManualID = 'MAN002'", $man2_pdf));
+
+    $wpdb->query("INSERT INTO {$wpdb->prefix}tblJoin (DateID, ProductID, ManualID)
+    VALUES
+      ('010501', 'C08-001-001-01-1-1', 'MAN001'),
+      ('020501', 'C05-35-02', 'MAN002')");
 }
 
 // Deactivation hook
@@ -93,5 +150,10 @@ function my_plugin_deactivate()
 {
     // Code to be executed on plugin deactivation
 }
-
+// Uninstall hook
+register_uninstall_hook(__FILE__, 'my_plugin_uninstall');
+function my_plugin_uninstall()
+{
+    // Code to be executed on plugin uninstallation
+}
 ?>
