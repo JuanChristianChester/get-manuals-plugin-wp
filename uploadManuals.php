32f849abb38af29e62c41c0ed39667709513cb60
ItchyPrune <94751482+ItchyPrune@users.noreply.github.com>
2023-05-10 11:42:52 +0100

    Update uploadManuals.php

--- uploadManuals.php
+++ uploadManuals.php
@@ -8,66 +8,77 @@ add_shortcode('manual_uploader', 'display_manual_uploader_form');
 function display_manual_uploader_form()
 {
     global $wpdb;
+    $error = '';
+    $success = false;
 
-    $message = '';
     if (isset($_POST['submit'])) {
+        // Get the last manual ID from the database
+        $last_manual_id = $wpdb->get_var("SELECT ManualID FROM {$wpdb->prefix}tblManuals ORDER BY ManualID DESC LIMIT 1");
+
+        // Increment the last manual ID to get the new manual ID
+        $new_manual_id = 'MAN-' . str_pad((intval(substr($last_manual_id, 4)) + 1), 4, '0', STR_PAD_LEFT);
+
+        // Get form data
         $serial_number = sanitize_text_field($_POST['serial_number']);
         $product_code = sanitize_text_field($_POST['product_code']);
+        $pdf_file = $_FILES['pdf_file'];
 
-        // Check if the serial number and product code are already in the database
-        $manual_exists = $wpdb->get_var($wpdb->prepare(
-            "SELECT COUNT(*) FROM {$wpdb->prefix}tblJoin WHERE DateID = %s AND ProductID = %s",
-            date('ymd'),
-            $product_code
-        ));
-
-        if ($manual_exists) {
-            $message = 'A manual for this product and date already exists in the database.';
+        // Validate form data
+        if (!$serial_number || !$product_code) {
+            $error = 'Please enter serial number and product code';
+        } elseif (!$pdf_file || $pdf_file['error'] !== UPLOAD_ERR_OK) {
+            $error = 'Please select a PDF file to upload';
         } else {
-            // Save the manual to the database
-            $manual_id = uniqid();
-            $filename = sanitize_file_name($_FILES['manual_file']['name']);
-            $pdf_data = file_get_contents($_FILES['manual_file']['tmp_name']);
+            // Read PDF file
+            $pdf_data = file_get_contents($pdf_file['tmp_name']);
+            if (!$pdf_data) {
+                $error = 'Failed to read PDF file';
+            } else {
+                // Insert manual into database
+                $wpdb->query(
+                    $wpdb->prepare(
+                        "INSERT INTO {$wpdb->prefix}tblManuals (ManualID, filename, pdf) VALUES (%s, %s, %s)",
+                        $new_manual_id,
+                        $pdf_file['name'],
+                        $pdf_data
+                    )
+                );
 
-            $wpdb->query($wpdb->prepare(
-                "INSERT INTO {$wpdb->prefix}tblManuals (ManualID, filename, pdf) VALUES (%s, %s, %s)",
-                $manual_id,
-                $filename,
-                $pdf_data
-            ));
+                // Insert join data into database
+                $wpdb->query(
+                    $wpdb->prepare(
+                        "INSERT INTO {$wpdb->prefix}tblJoin (DateID, ProductID, ManualID) VALUES (%s, %s, %s)",
+                        date('ymd'),
+                        $product_code,
+                        $new_manual_id
+                    )
+                );
 
-            $wpdb->query($wpdb->prepare(
-                "INSERT INTO {$wpdb->prefix}tblJoin (DateID, ProductID, ManualID) VALUES (%s, %s, %s)",
-                date('ymd'),
-                $product_code,
-                $manual_id
-            ));
-
-            $message = 'Manual uploaded successfully!';
+                $success = true;
+            }
         }
     }
-
-    ob_start();
 ?>
-    <form method="post" enctype="multipart/form-data">
-        <label for="serial_number">Serial Number:</label>
-        <input type="text" name="serial_number" id="serial_number">
-        <br><br>
-        <label for="product_code">Product Code:</label>
-        <input type="text" name="product_code" id="product_code">
-        <br><br>
-        <label for="manual_file">Manual File:</label>
-        <input type="file" name="manual_file" id="manual_file">
-        <br><br>
-        <input type="submit" name="submit" value="Upload">
-    </form>
-
-    <?php if ($message) { ?>
-        <p><?php echo $message; ?></p>
+    <?php if ($success) { ?>
+        <p>Manual uploaded successfully.</p>
+    <?php } else { ?>
+        <?php if ($error) { ?>
+            <p><?php echo $error; ?></p>
+        <?php } ?>
+        <form method="post" action="" enctype="multipart/form-data">
+            <label for="serial_number">Serial Number:</label>
+            <input type="text" name="serial_number" id="serial_number">
+            <br><br>
+            <label for="product_code">Product Code:</label>
+            <input type="text" name="product_code" id="product_code">
+            <br><br>
+            <label for="pdf_file">Select PDF File:</label>
+            <input type="file" name="pdf_file" id="pdf_file">
+            <br><br>
+            <input type="submit" name="submit" value="Upload">
+        </form>
     <?php } ?>
-
 <?php
-    return ob_get_clean();
 }
 
 ?>
