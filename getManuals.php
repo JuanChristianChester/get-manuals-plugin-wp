c1ea33120745c0dc164432ac5550682f95aa9fde
Juan Christian <Juanchristian2002@outlook.com>
2023-05-11 12:10:39 +0100

    Update getManuals.php

--- getManuals.php
+++ getManuals.php
@@ -48,8 +48,8 @@ function display_serial_number_search_form()
             <li><strong>Product Code:</strong> <?php echo $manual->ProductCode; ?></li>
             <li><strong>Manual:</strong>
                 <?php $pdf_url = wp_upload_dir()['baseurl'] . '/pdfs/' . $manual; ?>
-
-                <?php if (file_exists($pdf_url)) { ?>
+                <?php $filepath = 'wp-content/uploads/pdfs/' . $manual; ?>
+                <?php if (file_exists($filepath)) { ?>
                     <a href="<?php echo $pdf_url; ?>" target="_blank"><?php echo $manual; ?></a>
                 <?php } else { ?>
                     No manual available.
