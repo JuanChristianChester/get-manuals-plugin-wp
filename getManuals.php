52f1e2f0f7bb00d39f13376dc4de67ba1d44bac1
ItchyPrune <94751482+ItchyPrune@users.noreply.github.com>
2023-05-11 11:19:45 +0100

    test get Manuals

--- getManuals.php
+++ getManuals.php
@@ -12,6 +12,54 @@ License: GPL2
 // Shortcode to display the search form
 add_shortcode('serial_number_search_form', 'display_serial_number_search_form');
 
+function display_serial_number_search_form()
+{
+    $manual = null;
+    $error = '';
+
+    if (isset($_POST['submit'])) {
+        $serial_number = $_POST['serial_number'];
+        $product_code = $_POST['product_code'];
+        $manual = get_manual_by_serial_number_and_product_code($serial_number, $product_code);
+        if (!$manual) {
+            $error = 'Serial number and/or product code not found. Please enter valid ones.';
+        }
+    }
+?>
+    <form method="post" action="">
+        <label for="serial_number">Enter Serial Number:</label>
+        <input type="text" name="serial_number" id="serial_number">
+        <br><br>
+        <label for="product_code">Enter Product Code:</label>
+        <input type="text" name="product_code" id="product_code">
+        <br><br>
+        <input type="submit" name="submit" value="Search">
+    </form>
+
+    <?php if ($error) { ?>
+        <p><?php echo $error; ?></p>
+    <?php } ?>
+
+    <?php if ($manual) { ?>
+        <h3>Manual Details</h3>
+        <ul>
+            <li><strong>Serial Number:</strong> <?php echo $manual->SerialNumber; ?></li>
+            <li><strong>Product Code:</strong> <?php echo $manual->ProductCode; ?></li>
+            <li><strong>Manual:</strong>
+                <?php $pdf_url = 'wp-content/uploads/pdfs/' . $manual; ?>
+                <?php if (file_exists($pdf_url)) { ?>
+                    <a href="<?php echo $pdf_url; ?>" target="_blank"><?php echo $manual; ?></a>
+                <?php } else { ?>
+                    No manual available.
+                <?php } ?>
+            </li>
+        </ul>
+    <?php } ?>
+<?php
+}
+
+
+/*
 /// Function to display the search form and manual details
 function display_serial_number_search_form()
 {
@@ -58,9 +106,9 @@ function display_serial_number_search_form()
     <?php } ?>
 <?php
 }
+*/
 
 //Function to search the DB for the serial number and return the manual
-
 function get_manual_by_serial_number_and_product_code($serial_number, $product_code)
 {
     global $wpdb;
