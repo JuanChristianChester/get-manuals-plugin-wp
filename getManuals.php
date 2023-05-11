4ce1b2a6ad79607550761b53ec1dfa59b3bcfbb8
ItchyPrune <94751482+ItchyPrune@users.noreply.github.com>
2023-05-11 12:50:30 +0100

    Update getManuals.php

--- getManuals.php
+++ getManuals.php
@@ -95,6 +95,7 @@ register_activation_hook(__FILE__, 'my_plugin_activate');
 function my_plugin_activate()
 {
     global $wpdb;
+
     // Drop tables if they exist
     $wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}tblJoin");
     $wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}tblManuals");
@@ -102,39 +103,41 @@ function my_plugin_activate()
 
     // Create tables if they don't exist
     $wpdb->query("CREATE TABLE IF NOT EXISTS {$wpdb->prefix}tblDate (
-      DateID varchar(6) PRIMARY KEY,
-      Date date
-    )");
+  DateID varchar(6) PRIMARY KEY,
+  Date date
+)");
 
     $wpdb->query("CREATE TABLE IF NOT EXISTS {$wpdb->prefix}tblManuals (
-      ManualID varchar(15) PRIMARY KEY,
-      filename VARCHAR(255),
-    )");
+  ManualID varchar(15) PRIMARY KEY,
+  filename VARCHAR(255)
+)");
 
     $wpdb->query("CREATE TABLE IF NOT EXISTS {$wpdb->prefix}tblJoin (
-      DateID VARCHAR(6),
-      ProductID VARCHAR(30),
-      ManualID varchar(15),
-      PRIMARY KEY (DateID, ProductID, ManualID)
-    )");
+  DateID VARCHAR(6),
+  ProductID VARCHAR(30),
+  ManualID varchar(15),
+  PRIMARY KEY (DateID, ProductID, ManualID)
+)");
 
     // Insert data
     $wpdb->query("INSERT INTO {$wpdb->prefix}tblDate (DateID, Date)
-    VALUES
-      ('010501', '2001-05-01'),
-      ('011201', '2001-12-01'),
-      ('020501', '2002-05-01')");
-
+  VALUES
+    ('010501', '2001-05-01'),
+    ('011201', '2001-12-01'),
+    ('020501', '2002-05-01')
+");
 
     $wpdb->query("INSERT INTO {$wpdb->prefix}tblManuals (ManualID, filename) 
-    VALUES 
-  ('MAN-0001', 'Man1.pdf'), 
-  ('MAN-0002', 'Man2.pdf')");
+  VALUES 
+    ('MAN-0001', 'Man1.pdf'), 
+    ('MAN-0002', 'Man2.pdf')
+");
 
     $wpdb->query("INSERT INTO {$wpdb->prefix}tblJoin (DateID, ProductID, ManualID)
-    VALUES
-  ('010501', 'C08-001-001-01-1-1', 'MAN-0001'),
-  ('020501', 'C05-35-02', 'MAN-0002')");
+  VALUES
+    ('010501', 'C08-001-001-01-1-1', 'MAN-0001'),
+    ('020501', 'C05-35-02', 'MAN-0002')
+");
 }
 
 // Deactivation hook
